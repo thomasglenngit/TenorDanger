83f9997262d5bc7362102dd26bcb2d8e8f20bc21
Thomas Glenn <thomasgrantglenn@mac.com>
2020-10-28 02:01:48 -0600

    add tenor danger.

---
+++ Assets/TenorNotes.cs
@@ -0,0 +1,159 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TenorNotes : MonoBehaviour
+{
+   public AudioSource C2_Note;
+   public AudioSource D2_Note;
+   public AudioSource E2_Note;
+   public AudioSource F2_Note;
+   public AudioSource G2_Note;
+   public AudioSource A3_Note;
+   public AudioSource B3_Note;
+   public AudioSource C3_Note;
+   public AudioSource D3_Note;
+   public AudioSource E3_Note;
+   public AudioSource F3_Note;
+   public AudioSource G3_Note;
+   public AudioSource A4_Note;
+   public AudioSource B4_Note;
+   public AudioSource C4_Note;
+   public AudioSource CS2_Note;
+   public AudioSource DS2_Note;
+   public AudioSource FS2_Note;
+   public AudioSource GS2_Note;
+   public AudioSource AS3_Note;
+   public AudioSource CS3_Note;
+   public AudioSource DS3_Note;
+   public AudioSource FS3_Note;
+   public AudioSource GS3_Note;
+   public AudioSource AS4_Note;
+
+   public void C2_Note_Play()
+   {
+       C2_Note.Play();
+   }
+   
+   public void D2_Note_Play()
+   {
+       D2_Note.Play();
+   }
+
+    public void E2_Note_Play()
+   {
+       E2_Note.Play();
+   }
+
+    public void F2_Note_Play()
+   {
+       F2_Note.Play();
+   }
+
+    public void G2_Note_Play()
+   {
+       G2_Note.Play();
+   }
+
+    public void A3_Note_Play()
+   {
+       A3_Note.Play();
+   }
+
+    public void B3_Note_Play()
+   {
+       B3_Note.Play();
+   }
+
+    public void C3_Note_Play()
+   {
+       C3_Note.Play();
+   }
+
+    public void D3_Note_Play()
+   {
+       D3_Note.Play();
+   }
+
+    public void E3_Note_Play()
+   {
+       E3_Note.Play();
+   }
+
+    public void F3_Note_Play()
+   {
+       F3_Note.Play();
+   }
+
+    public void G3_Note_Play()
+   {
+       G3_Note.Play();
+   }
+
+    public void A4_Note_Play()
+   {
+       A4_Note.Play();
+   }
+
+    public void B4_Note_Play()
+   {
+       B4_Note.Play();
+   }
+
+    public void C4_Note_Play()
+   {
+       C4_Note.Play();
+   }
+
+    public void CS2_Note_Play()
+   {
+       CS2_Note.Play();
+   }
+
+    public void DS2_Note_Play()
+   {
+       DS2_Note.Play();
+   }
+
+    public void FS2_Note_Play()
+   {
+       FS2_Note.Play();
+   }
+
+    public void GS2_Note_Play()
+   {
+       GS2_Note.Play();
+   }
+
+    public void AS3_Note_Play()
+   {
+       AS3_Note.Play();
+   }
+
+    public void CS3_Note_Play()
+   {
+       CS3_Note.Play();
+   }
+
+    public void DS3_Note_Play()
+   {
+       DS3_Note.Play();
+   }
+
+    public void FS3_Note_Play()
+   {
+       FS3_Note.Play();
+   }
+
+    public void GS3_Note_Play()
+   {
+       GS3_Note.Play();
+   }
+
+    public void AS4_Note_Play()
+   {
+       AS4_Note.Play();
+   }
+
+
+}
